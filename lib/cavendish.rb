2732dac762e8cb571f53ff9ab18ca5ee7f64302f
Ignacio Acosta <iaacosta@uc.cl>
2021-07-25 12:41:13 -0400

    feat(commands): create base powertypes command for steps
    and automatically require all commands

--- lib/cavendish.rb
+++ lib/cavendish.rb
@@ -1,6 +1,11 @@
 require "commander"
 require "colorize"
+require "power-types"
+
 require "cavendish/version"
+
+require "cavendish/commands/base"
+Dir[File.join(__dir__, 'cavendish', 'commands', '*.rb')].each { |path| require_relative path }
 
 module Cavendish
 end
