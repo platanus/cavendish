f74e034ede5ac50549671ba49ade7de176237bb9
Ignacio Acosta <iaacosta@uc.cl>
2021-07-25 14:08:34 -0400

    feat(generator): add tailwind rn step

---
+++ lib/cavendish/commands/add_tailwind.rb
@@ -0,0 +1,31 @@
+module Cavendish
+  module Commands
+    class AddTailwind < Cavendish::Commands::Base
+      def perform
+        install_tailwind_rn_dependencies
+        copy_tailwind_config_file
+        generate_tailwind_rn_styles_json
+        copy_tailwind_utils
+      end
+
+      private
+
+      def install_tailwind_rn_dependencies
+        run_in_project('yarn add tailwind-rn')
+        run_in_project('yarn add -D tailwindcss')
+      end
+
+      def copy_tailwind_config_file
+        copy_file('tailwind.config.js', 'tailwind.config.js')
+      end
+
+      def generate_tailwind_rn_styles_json
+        run_in_project('yarn run create-tailwind-rn')
+      end
+
+      def copy_tailwind_utils
+        copy_file('src/utils/tailwindRn.js', 'src/utils/tailwindRn.js')
+      end
+    end
+  end
+end
