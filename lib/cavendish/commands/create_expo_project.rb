9cd12536b3323f430cb8b0d4f20d24f0162d3ebc
Matías Montagna <matias@platan.us>
2023-02-10 11:26:19 -0300

    chore(expo-cli): remove expo-cli install

--- lib/cavendish/commands/create_expo_project.rb
+++ lib/cavendish/commands/create_expo_project.rb
@@ -2,15 +2,10 @@ module Cavendish
   module Commands
     class CreateExpoProject < Cavendish::Commands::Base
       def perform
-        install_expo_globally
         create_expo_project
       end
 
       private
-
-      def install_expo_globally
-        run("yarn global add expo-cli@#{Cavendish::EXPO_CLI_VERSION}")
-      end
 
       def create_expo_project
         run("npx create-expo-app #{config.project_name} --template blank")
