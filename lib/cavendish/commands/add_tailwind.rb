b1d5defa942441fdc1507ee941523a79d8d00dab
Matías Montagna <matias@platan.us>
2023-02-10 17:08:17 -0300

    chore(tailwind): update tailwind setup

--- lib/cavendish/commands/add_tailwind.rb
+++ lib/cavendish/commands/add_tailwind.rb
@@ -3,16 +3,17 @@ module Cavendish
     class AddTailwind < Cavendish::Commands::Base
       def perform
         install_tailwind_rn_dependencies
-        copy_tailwind_config_file
-        generate_tailwind_rn_styles_json
-        copy_tailwind_utils
+        run_setup_script
       end
 
       private
 
       def install_tailwind_rn_dependencies
         run_in_project('yarn add tailwind-rn')
-        run_in_project("yarn add -D tailwindcss@#{Cavendish::TAILWIND_VERSION}")
+      end
+
+      def run_setup_script
+        run_in_project('npx setup-tailwind-rn')
       end
 
       def copy_tailwind_config_file
