7f1c06c29fe8e451096e8becc9a000bf67888473
Ignacio Acosta <iaacosta@uc.cl>
2023-02-10 17:12:57 -0300

    feat(aliases): add alias to example file imports

--- lib/cavendish/assets/src/navigators/HomeNavigator.tsx
+++ lib/cavendish/assets/src/navigators/HomeNavigator.tsx
@@ -1,6 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from '../screens/HomeScreen';
+import HomeScreen from '@/screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
