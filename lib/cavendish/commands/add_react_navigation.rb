536e88ddbdb2cf9146f518eb02ff4b8174c1c0cd
Ignacio Acosta <iaacosta@uc.cl>
2023-02-20 16:04:20 -0300

    refactor(react-navigation): move template files to other command

--- lib/cavendish/commands/add_react_navigation.rb
+++ lib/cavendish/commands/add_react_navigation.rb
@@ -3,8 +3,6 @@ module Cavendish
     class AddReactNavigation < Cavendish::Commands::Base
       def perform
         install_dependencies
-        add_example_navigator_and_screens
-        replace_app_entrypoint
       end
 
       private
@@ -12,16 +10,6 @@ module Cavendish
       def install_dependencies
         run_in_project("yarn add #{react_navigation_core_dependencies.join(' ')}")
         run_in_project("npx expo install #{react_navigation_side_dependencies.join(' ')}")
-      end
-
-      def add_example_navigator_and_screens
-        copy_template('src/screens/HomeScreen.tsx', 'src/screens/HomeScreen.tsx')
-        copy_file('src/navigators/HomeNavigator.tsx', 'src/navigators/HomeNavigator.tsx')
-      end
-
-      def replace_app_entrypoint
-        remove_in_project('App.tsx')
-        copy_file('App.tsx', 'App.tsx')
       end
 
       def react_navigation_core_dependencies
--- lib/cavendish/commands/add_template_files.rb
+++ lib/cavendish/commands/add_template_files.rb
@@ -3,7 +3,9 @@ module Cavendish
     class AddTemplateFiles < Cavendish::Commands::Base
       def perform
         copy_tailwind_config_file
-        copy_tailwind_provider_file
+        copy_provider_file
+        copy_sample_navigator_and_screens
+        replace_app_entrypoint
       end
 
       private
@@ -12,8 +14,18 @@ module Cavendish
         copy_file('tailwind.config.js', 'tailwind.config.js')
       end
 
-      def copy_tailwind_provider_file
+      def copy_provider_file
         copy_file('src/providers/AppProvider.tsx', 'src/providers/AppProvider.tsx')
+      end
+
+      def copy_sample_navigator_and_screens
+        copy_template('src/screens/HomeScreen.tsx', 'src/screens/HomeScreen.tsx')
+        copy_file('src/navigators/HomeNavigator.tsx', 'src/navigators/HomeNavigator.tsx')
+      end
+
+      def replace_app_entrypoint
+        remove_in_project('App.tsx')
+        copy_file('App.tsx', 'App.tsx')
       end
     end
   end
