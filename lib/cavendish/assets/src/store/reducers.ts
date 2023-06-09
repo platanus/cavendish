1fa275d7df1343d7a3aec32d28f5165097cd77e3
Matías Montagna <matias@platan.us>
2023-06-09 17:52:33 -0400

    feat(store/reducers): add example in file comments

--- lib/cavendish/assets/src/store/reducers.ts
+++ lib/cavendish/assets/src/store/reducers.ts
@@ -3,3 +3,43 @@ import { combineReducers } from '@reduxjs/toolkit';
 const appReducer = combineReducers({});
 
 export default appReducer;
+
+/*
+// Example on how to create a counter slice in the path `src/store/slices/counter.ts`
+
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    reset(state) {
+      state.value = 0;
+    },
+    incrementBy(state, action: PayloadAction<number>) {
+      state.value += action.payload;
+    },
+    decrementBy(state, action: PayloadAction<number>) {
+      state.value -= action.payload;
+    },
+  },
+});
+
+
+// then to connect the slice, in this file do
+
+import { combineReducers } from '@reduxjs/toolkit';
+
+import { counterSlice } from @/store/slices/counter;
+
+const appReducer = combineReducers({
+  counter: counterSlice.reducer,
+});
+
+export default appReducer;
+
+*/
